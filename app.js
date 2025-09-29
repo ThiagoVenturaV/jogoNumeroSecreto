cb8c4b0fbd01469a74967315bca61749fbbda30a
ThiagoVenturaV <83434844+ThiagoVenturaV@users.noreply.github.com>
2025-09-29 20:54:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-alert('Bem-Vindo ao jogo do número secreto!');
-let numeroSecreto = 7;
-console.log(numeroSecreto);
-let chute = prompt('Escolha um múmero entre 1 e 10:');
-console.log('valor do chute:',chute);
-console.log('comparação:',chute==numeroSecreto);
-//se o chute for igual ao numero secreto
-if(chute==numeroSecreto){
-    alert(`isso ai, Você acertou é ${numeroSecreto}!`);
-}else{
-    console.log('valor do numero secreto:',numeroSecreto);
-    alert('você errou :(\nO número secreto era '+numeroSecreto);
+alert(`Boas vindas ao jogo do numero secreto`);
+let numeroSecreto = 2;
+console.log(numeroSecreto)
+let chute;
+
+while(chute!=numeroSecreto){
+    chute = prompt(`Escolha um numero entre 1 e 10`);
+    if (chute== numeroSecreto){
+        alert(`Isso aí! você descobriu o njumero secreto ${numeroSecreto}`);
+    }else{
+        if (chute > numeroSecreto){
+            alert(`o numero secreto é menor que ${chute}`);
+        }else{
+            alert(`o numero secreto é maior que ${chute}`);
+        }
+}
+
+
 }

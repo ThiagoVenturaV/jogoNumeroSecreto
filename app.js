396908b39c39c8f5351089aa92445c74635fe25d
ThiagoVenturaV <83434844+ThiagoVenturaV@users.noreply.github.com>
2025-10-07 20:43:21 -0300

    Atualizar o app.js
    Numero corrigido

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let chute;
 while(chute!=numeroSecreto){
     chute = prompt(`Escolha um numero entre 1 e 10`);
     if (chute== numeroSecreto){
-        alert(`Isso aí! você descobriu o njumero secreto ${numeroSecreto}`);
+        alert(`Isso aí! você descobriu o numero secreto ${numeroSecreto}`);
     }else{
         if (chute > numeroSecreto){
             alert(`o numero secreto é menor que ${chute}`);
